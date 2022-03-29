915c2cb4947af0ee7ebb9c93a8d159890e2f3803
Marcio Endo <marcio@objectos.com.br>
2022-03-29 11:59:26 -0300

    [#13] add link to home (desktop)
    Closes objectos/objectos#13
    
    $source=735c268cc02bd397e748c1bf5a42421c31efafd9$

---
+++ docs/src/main/java17/br/com/objectos/docs/Breakpoint.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2022-2022 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package br.com.objectos.docs;
+
+import br.com.objectos.css.parser.sheet.Length;
+
+public final class Breakpoint {
+
+  public static final Length LG = Length.px(1024);
+
+  public static final Length MD = Length.px(768);
+
+  public static final Length SM = Length.px(640);
+
+  public static final Length XL = Length.px(1280);
+
+  public static final Length XXL = Length.px(1536);
+
+  private Breakpoint() {}
+
+}
