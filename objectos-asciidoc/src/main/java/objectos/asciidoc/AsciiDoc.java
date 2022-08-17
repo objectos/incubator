ec2e3b361d5524a3e286e7190f10bec4dad64bba
Marcio Endo <marcio@objectos.com.br>
2022-08-17 11:43:59 -0300

    [#78] revert changes
    After some testing in objectos-docs this would not achieve
    the intended goals.
    
    So revering back.
    
    $source=62a679883adedd2e99ddfa5eda62456b03823ff6$

--- objectos-asciidoc/src/main/java/objectos/asciidoc/AsciiDoc.java
+++ objectos-asciidoc/src/main/java/objectos/asciidoc/AsciiDoc.java
@@ -15,9 +15,11 @@
  */
 package objectos.asciidoc;
 
+import java.util.Map;
 import objectos.lang.Check;
+import objectos.util.GrowableMap;
 
-public class AsciiDoc extends Interpreter {
+public class AsciiDoc {
 
   public interface Processor {
 
@@ -81,9 +83,23 @@ public class AsciiDoc extends Interpreter {
 
   }
 
+  private static class ListingBlock {
+    static final int SOURCE_CODE = 1 << 0;
+  }
+
+  private final GrowableMap<String, AttrValue> attrMap = new GrowableMap<>();
+
   private final Pass0 pass0 = new Pass0();
 
   private final Pass1 pass1 = new Pass1();
+
+  private final Pass2 pass2 = new Pass2();
+
+  private String source;
+
+  private Processor processor;
+
+  private int state;
 
   private AsciiDoc() {
   }
@@ -92,56 +108,337 @@ public class AsciiDoc extends Interpreter {
     return new AsciiDoc();
   }
 
-  public final Document parse(String source) {
-    this.source = Check.notNull(source, "source == null");
-
-    pass0.execute(source);
-
-    pass1.execute(pass0);
-
-    int[] tokens = pass0.toToken();
-
-    int[] codes = pass1.toCode();
-
-    var attributes = pass1.toDocumentAttributes();
-
-    pass1.running = false;
-
-    return new Document(
-      source, attributes, tokens, codes
-    );
-  }
-
   public final void process(String source, Processor processor) {
     this.source = Check.notNull(source, "source == null");
     this.processor = Check.notNull(processor, "processor == null");
 
     pass0.execute(source);
 
-    pass2Source = pass0;
-
     pass1.execute(pass0);
-
-    attributes = pass1.toDocumentAttributes();
 
     process0();
 
     pass1.running = false;
   }
 
-  @Override
-  final int codeAt(int index) { return pass1.codeAt(index); }
+  final void process0() {
+    while (hasCode()) {
+      int code = nextCode();
 
-  @Override
-  final int codeCursor() { return pass1.codeCursor(); }
+      switch (code) {
+        case Code.DOCUMENT_START -> processor.documentStart(pass1.toDocumentAttributes());
 
-  @Override
-  final boolean hasCode() { return pass1.hasCode(); }
+        case Code.DOCUMENT_END -> processor.documentEnd();
 
-  @Override
-  final int nextCode() { return pass1.nextCode(); }
+        case Code.HEADING_START -> processor.headingStart(nextCode());
 
-  @Override
-  final int tokenAt(int index) { return pass0.tokenAt(index); }
+        case Code.HEADING_END -> processor.headingEnd();
+
+        case Code.PREAMBLE_START -> processor.preambleStart();
+
+        case Code.PREAMBLE_END -> processor.preambleEnd();
+
+        case Code.SECTION_START -> processSectionStart(nextCode());
+
+        case Code.SECTION_END -> { processor.sectionEnd(); attrMap.clear(); }
+
+        case Code.PARAGRAPH_START -> processor.paragraphStart();
+
+        case Code.PARAGRAPH_END -> { processor.paragraphEnd(); attrMap.clear(); }
+
+        case Code.TOKENS -> processTokens(nextCode(), nextCode());
+
+        case Code.ATTR_POSITIONAL -> processAttrPositional(nextCode(), nextCode(), nextCode());
+
+        case Code.ATTR_NAMED -> processAttrNamed(nextCode(), nextCode(), nextCode(), nextCode());
+
+        case Code.LISTING_BLOCK_START -> processListingBlockStart();
+
+        case Code.LISTING_BLOCK_END -> { processListingBlockEnd(); attrMap.clear(); }
+
+        case Code.VERBATIM -> processVerbatim(nextCode(), nextCode());
+
+        case Code.ULIST_START -> processor.unorderedListStart();
+
+        case Code.ULIST_END -> { processor.unorderedListEnd(); attrMap.clear(); }
+
+        case Code.LI_START -> processor.listItemStart();
+
+        case Code.LI_END -> processor.listItemEnd();
+
+        case Code.INLINE_MACRO -> { processMacroInline(); attrMap.clear(); }
+
+        case Code.URL_MACRO -> { processMacroUrl(); attrMap.clear(); }
+
+        default -> throw new UnsupportedOperationException("Implement me :: code=" + code);
+      }
+    }
+  }
+
+  private boolean hasCode() { return pass1.hasCode(); }
+
+  private int nextCode() { return pass1.nextCode(); }
+
+  private void processAttrNamed(int nameStart, int nameEnd, int first, int last) {
+    var name = source.substring(nameStart, nameEnd);
+
+    processAttrPositionalOrNamed(name, first, last);
+  }
+
+  private void processAttrPositional(int position, int first, int last) {
+    var name = Integer.toString(position);
+
+    processAttrPositionalOrNamed(name, first, last);
+  }
+
+  private void processAttrPositionalOrNamed(String name, int first, int last) {
+    pass2.execute(pass0, first, last);
+
+    AttrValue value = pass2.toAttrValue(source);
+
+    attrMap.put(name, value);
+
+    pass2.reset();
+  }
+
+  private void processListingBlockEnd() {
+    switch (state) {
+      case ListingBlock.SOURCE_CODE -> processor.sourceCodeBlockEnd();
+
+      default -> processor.listingBlockEnd();
+    }
+  }
+
+  private void processListingBlockStart() {
+    if (attrMap.isEmpty()) {
+      processor.listingBlockStart();
+
+      return;
+    }
+
+    var style = stringAttr(AttrName.STYLE, "1");
+
+    switch (style) {
+      case "" -> processListingBlockStartMaybeSource();
+
+      case "source" -> processSourceCodeBlock();
+
+      default -> throw new UnsupportedOperationException("Implement me :: state=" + state);
+    }
+  }
+
+  private void processListingBlockStartMaybeSource() {
+    var lang = stringAttr(AttrName.LANGUAGE, "2");
+
+    if (lang.equals("")) {
+      throw new UnsupportedOperationException("Implement me :: no lang found");
+    }
+
+    processSourceCodeBlock(lang);
+  }
+
+  private void processMacroInline() {
+    var name = source.substring(nextCode(), nextCode());
+
+    var maybeTarget = nextCode();
+
+    if (maybeTarget != Code.MACRO_TARGET) {
+      throw new UnsupportedOperationException("Implement me");
+    }
+
+    var target = source.substring(nextCode(), nextCode());
+
+    processMacroInlineAttrMap();
+
+    processMacroInlineAny(name, target);
+  }
+
+  private void processMacroInlineAny(String name, String target) {
+    class Impl implements InlineMacroAttributes {
+      final Map<String, AttrValue> copy = attrMap.toUnmodifiableMap();
+
+      @Override
+      public final void render(String name) {
+        Check.notNull(name, "name == null");
+
+        var attr = copy.getOrDefault(name, AttrValue.EMPTY);
+
+        attr.render(processor);
+      }
+    }
+
+    processor.inlineMacro(name, target, new Impl());
+  }
+
+  private void processMacroInlineAttrMap() {
+    attrMap.clear();
+
+    loop: while (hasCode()) {
+      var cursor = pass1.codeCursor();
+
+      var code = pass1.codeAt(cursor);
+
+      switch (code) {
+        case Code.ATTR_POSITIONAL -> {
+          nextCode();
+
+          processAttrPositional(nextCode(), nextCode(), nextCode());
+        }
+
+        default -> { break loop; }
+      }
+    }
+  }
+
+  private void processMacroUrl() {
+    var href = source.substring(nextCode(), nextCode());
+
+    var start = nextCode();
+
+    if (start != Code.URL_TARGET_START) {
+      throw new UnsupportedOperationException(
+        "Implement me :: expected Code.URL_TARGET_START but found=" + start);
+    }
+
+    var codeStart = pass1.codeCursor();
+
+    var end = Integer.MAX_VALUE;
+
+    do {
+      end = nextCode();
+    } while (hasCode() && end != Code.URL_TARGET_END);
+
+    if (end == Integer.MAX_VALUE) {
+      throw new UnsupportedOperationException("Implement me");
+    }
+
+    var codeEnd = pass1.codeCursor() - 1;
+
+    processor.link(href, () -> {
+      for (int index = codeStart; index < codeEnd;) {
+        var code = pass1.codeAt(index++);
+
+        switch (code) {
+          case Code.TOKENS -> {
+            var first = pass1.codeAt(index++);
+            var last = pass1.codeAt(index++);
+
+            processTokens(first, last);
+          }
+
+          default -> throw new UnsupportedOperationException("Implement me :: code=" + code);
+        }
+      }
+    });
+  }
+
+  private void processSectionStart(int level) {
+    if (attrMap.isEmpty()) {
+      processor.sectionStart(level);
+    } else {
+      processor.sectionStart(level);
+    }
+  }
+
+  private void processSourceCodeBlock() {
+    var lang = stringAttr(AttrName.LANGUAGE, "2");
+
+    if (lang.equals("")) {
+      throw new UnsupportedOperationException("Implement me :: no lang found");
+    }
+
+    processSourceCodeBlock(lang);
+  }
+
+  private void processSourceCodeBlock(String lang) {
+    state = ListingBlock.SOURCE_CODE;
+
+    processor.sourceCodeBlockStart(lang);
+  }
+
+  private void processTokens(int first, int last) {
+    pass2.execute(pass0, first, last);
+
+    while (pass2.hasText()) {
+      var text = pass2.nextText();
+
+      switch (text) {
+        case Text.REGULAR -> {
+          var begin = pass2.nextText();
+          var end = pass2.nextText();
+
+          text(begin, end);
+        }
+
+        case Text.MONOSPACE_START -> processor.monospaceStart();
+
+        case Text.MONOSPACE_END -> processor.monospaceEnd();
+
+        case Text.BOLD_START -> processor.boldStart();
+
+        case Text.BOLD_END -> processor.boldEnd();
+
+        case Text.ITALIC_START -> processor.italicStart();
+
+        case Text.ITALIC_END -> processor.italicEnd();
+
+        case Text.CURVED_APOSTROPHE -> {
+          processor.text("’");
+          //processor.text("'");
+        }
+
+        default -> throw new UnsupportedOperationException("Implement me :: text=" + text);
+      }
+    }
+
+    pass2.reset();
+  }
+
+  private void processVerbatim(int first, int last) {
+    for (int index = first; index < last;) {
+      int token = pass0.token(index++);
+
+      switch (token) {
+        case Token.BLOB, Token.LITERALI -> {
+          var begin = pass0.token(index++);
+          var end = pass0.token(index++);
+
+          text(begin, end);
+        }
+
+        case Token.BOLD_END -> {
+          var pos = pass0.token(index++);
+
+          text(pos, pos + 1);
+        }
+
+        case Token.LF -> processor.lineFeed();
+
+        default -> uoe(token);
+      }
+    }
+  }
+
+  private String stringAttr(String name, String alt) {
+    var attr = attrMap.get(name);
+
+    if (attr == null) {
+      attr = attrMap.getOrDefault(alt, AttrValue.EMPTY);
+    }
+
+    return attr.stringValue();
+  }
+
+  private void text(int begin, int end) {
+    if (begin < end) {
+      var s = source.substring(begin, end);
+
+      processor.text(s);
+    }
+  }
+
+  private int uoe(int value) {
+    throw new UnsupportedOperationException("Implement me :: value=" + value);
+  }
 
 }
