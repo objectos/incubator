19f1f7e85a171c7829a5680704f147b996918286
Marcio Endo <marcio@objectos.com.br>
2023-02-16 10:56:16 -0300

    [#208] work docs/v0004/objectos-code/java-template
    $source=0761111b36e5524b1bc0c04dadb9be34231979b3$

---
+++ docs/src/test/java/objectos/docs/code/JavaTemplateAutoImportsConflict.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2022-2023 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package objectos.docs.code;
+
+import objectos.code.JavaTemplate;
+
+// objectos-code/java-template.adoc
+public class JavaTemplateAutoImportsConflict extends JavaTemplate {
+
+  public static void main(String[] args) {
+    System.out.println(new JavaTemplateAutoImportsConflict());
+  }
+
+  @Override
+  protected final void definition() {
+    // @formatter:off
+    _package("com.example");
+
+    autoImports();
+
+    _public(); _class("AutoImports"); body(
+      t(t(java.awt.List.class), t(String.class)), id("a"),
+
+      t(t(java.util.List.class), t(String.class)), id("b"),
+
+      t(t("com.example", "List"), t(String.class)), id("c")
+    );
+  }
+
+}
---
+++ docs/src/test/java/objectos/docs/code/JavaTemplateAutoImportsOn.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2022-2023 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package objectos.docs.code;
+
+import java.io.IOException;
+import java.util.List;
+import objectos.code.JavaTemplate;
+
+// objectos-code/java-template.adoc
+public class JavaTemplateAutoImportsOn extends JavaTemplate {
+
+  public static void main(String[] args) {
+    System.out.println(new JavaTemplateAutoImportsOn());
+  }
+
+  @Override
+  protected final void definition() {
+    // @formatter:off
+    _package("com.example");
+
+    autoImports();
+
+    _public(); _class("AutoImports"); body(
+      t(t(List.class), t(IOException.class)), id("a"),
+
+      t(String.class), id("b")
+    );
+  }
+
+}
