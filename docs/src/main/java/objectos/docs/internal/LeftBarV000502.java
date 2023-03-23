2646669302bb21a85538740b8990eb944ecf37c4
Marcio Endo <marcio@objectos.com.br>
2023-03-23 14:11:29 -0300

    [#297] add stubs for throws and body
    Closes objectos/objectos#297
    
    $source=45b48229df24ac33f772dcb8f9f71a96472e763c$

--- docs/src/main/java/objectos/docs/internal/LeftBarV000502.java
+++ docs/src/main/java/objectos/docs/internal/LeftBarV000502.java
@@ -85,7 +85,9 @@ final class LeftBarV000502 extends LeftBarFragment {
             li(a0("v000502/objectos-code/method/modifiers")),
             li(a0("v000502/objectos-code/method/annotations")),
             li(a0("v000502/objectos-code/method/parameters")),
-            li(a0("v000502/objectos-code/method/type-parameters"))
+            li(a0("v000502/objectos-code/method/type-parameters")),
+            li(a0("v000502/objectos-code/method/throws")),
+            li(a0("v000502/objectos-code/method/body"))
           )
         ),
 
